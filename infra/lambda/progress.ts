a35c28e4c072ad6261d2e76332768ab92a6fd4a1
Mani <65660259+mani1261790@users.noreply.github.com>
2026-04-15 11:15:14 +0900

    feat: add progress lambda

---
+++ infra/lambda/progress.ts
@@ -0,0 +1,4 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand, QueryCommand } from "@aws-sdk/lib-dynamodb";
+
+// (content already created above)
