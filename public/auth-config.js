1de5add5f0e8fd11f10e3578b6d18ee7f2a91e67
Mani <mani1261790@gmail.com>
2026-05-10 09:18:55 +0900

    Fix Cognito auth config drift for assistant flows (#66)

---
+++ public/auth-config.js
@@ -0,0 +1,5 @@
+window.__NOEMA_AUTH_CONFIG__ = {
+  region: "ap-northeast-3",
+  clientId: "1tkarvhnc0bmo9ikn598afmg9",
+  cognitoDomain: "https://noema-437089831576-auth.auth.ap-northeast-3.amazoncognito.com"
+};
